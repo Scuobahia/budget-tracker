b682c83dbf78abcde076b7a706ff38886c08d759
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-09-07 13:19:30 -0600

    idb reference from module 18 and manifest.json setup

--- public/server-worker.js
+++ public/server-worker.js
@@ -3,21 +3,12 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/js/index.js",
-  "./public/css/style.css",
-  "./public/js/index.js",
-  "./public/manifest.json"
-//   "./events.html",
-//   "./tickets.html",
-//   "./schedule.html",
-//   "./assets/css/style.css",
-//   "./assets/css/bootstrap.css",
-//   "./assets/css/tickets.css",
-//   "./dist/app.bundle.js",
-//   "./dist/events.bundle.js",
-//   "./dist/tickets.bundle.js",
-//   "./dist/schedule.bundle.js"
+  "./index.html",
+  "./js/index.js",
+  "./css/style.css",
+  "./js/index.js",
+  "./manifest.json"
+
 ];
 
 // Respond with cached resources
